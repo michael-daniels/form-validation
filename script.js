241458404ee65d485a6ecf24b20580d35482a4b0
‘Michael <michael.daniels.jr@gmail.com>
2018-01-18 17:36:45 -0700

    update 3

--- script.js
+++ script.js
@@ -12,10 +12,10 @@
 
       if (fullNameInput.value.length > 3) {
         document.getElementById('result-message').innerHTML = "Name is ok!"
-        fullNameInput.style.backgroundColor = "#8BC34A"
+        fullNameInput.style.backgroundColor = "#99ff99"
       } else {
         document.getElementById('result-message').innerHTML = "Name isn't long enough!"
-        fullNameInput.style.backgroundColor = "#FBC02D"
+        fullNameInput.style.backgroundColor = "#ffff99"
       }
 
     })
@@ -24,10 +24,10 @@
 
         if (emailInput.value.includes("@") && emailInput.value.endsWith(".com")) {
           document.getElementById('result-message').innerHTML = "Email is ok!"
-          emailInput.style.backgroundColor = "#8BC34A"
+          emailInput.style.backgroundColor = "#99ff99"
         } else {
           document.getElementById('result-message').innerHTML = "Email doesn't appear valid!"
-          emailInput.style.backgroundColor = "#FBC02D"
+          emailInput.style.backgroundColor = "#ffff99"
         }
 
       })
@@ -36,10 +36,10 @@
 
           if (businessNameInput.value[0] === businessNameInput.value[0].toUpperCase()) {
             document.getElementById('result-message').innerHTML = "Biz name is ok!"
-            businessNameInput.style.backgroundColor = "#8BC34A"
+            businessNameInput.style.backgroundColor = "#99ff99"
           } else {
             document.getElementById('result-message').innerHTML = "Biz name must start with capital letter"
-            businessNameInput.style.backgroundColor = "#FBC02D"
+            businessNameInput.style.backgroundColor = "#ffff99"
           }
 
         })
@@ -48,10 +48,10 @@
 
             if (userNameInput.value.length > 4 && userNameInput.value.match(/\d/)) {
               document.getElementById('result-message').innerHTML = "Username is ok!"
-              userNameInput.style.backgroundColor = "#8BC34A"
+              userNameInput.style.backgroundColor = "#99ff99"
             } else {
               document.getElementById('result-message').innerHTML = "Username must be at least 5 characters in length and contain one number"
-              userNameInput.style.backgroundColor = "#FBC02D"
+              userNameInput.style.backgroundColor = "#ffff99"
             }
 
           })
@@ -60,10 +60,10 @@
 
               if (passwordInput.value.length > 5) {
                 document.getElementById('result-message').innerHTML = "Password is ok!"
-                passwordInput.style.backgroundColor = "#8BC34A";
+                passwordInput.style.backgroundColor = "#99ff99";
               } else {
                 document.getElementById('result-message').innerHTML = "Password must be at least 6 characters in length"
-                passwordInput.style.backgroundColor = "#FBC02D"
+                passwordInput.style.backgroundColor = "#ffff99"
               }
 
             })
@@ -72,10 +72,10 @@
 
                 if (verifyPasswordInput.value ===  passwordInput.value) {
                   document.getElementById('result-message').innerHTML = "Verify Password is ok!"
-                  verifyPasswordInput.style.backgroundColor = "#8BC34A";
+                  verifyPasswordInput.style.backgroundColor = "#99ff99";
                 } else {
                   document.getElementById('result-message').innerHTML = "Passwords must match"
-                  verifyPasswordInput.style.backgroundColor = "#FBC02D"
+                  verifyPasswordInput.style.backgroundColor = "#ffff99"
                 }
 
               })
